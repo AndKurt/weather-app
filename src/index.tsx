51a3519bfe29522759cba2c04679225e92af8c4e
AndKurt <kurtenkov.as@gmail.com>
2022-08-09 12:38:34 +0300

    feat: global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import { HomePage } from '@pages/HomePage'
+import { GlobalStyle } from '@styles/GlobalStyle'
+import { theme } from '@styles/theme'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<React.StrictMode>
-		<HomePage />
+  <React.StrictMode>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <HomePage />
+    </ThemeProvider>
   </React.StrictMode>
 )
